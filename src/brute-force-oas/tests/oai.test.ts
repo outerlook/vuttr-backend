8db89d2c45ce9ce1490fe613287b55aeee328cc6
Raffael Campos <raffael.rcampos@gmail.com>
2023-02-21 17:02:07 -0300

    try to setup a initial idea on generating openAPI from files, but already creating a openAPI specification upfront

---
+++ src/brute-force-oas/tests/oai.test.ts
@@ -0,0 +1,43 @@
+import { describe, expect, test } from "vitest";
+import { routesConfig } from "src/build-lambdas/generator-scripts/config";
+import oaiJSON from "../openAPI.json";
+
+/**
+ * /tools/{id} -> /tools/:id
+ */
+const formatVariable = (path: string) => {
+  return path.replace(/{/g, ":").replace(/}/g, "");
+};
+
+describe("oai", () => {
+  test("All routes are present in the routes config", () => {
+    // get all paths from the openAPI.json in the format {METHOD} {PATH}
+    const oaiPaths = Object.entries(oaiJSON.paths)
+      .map(([path, methods]) => {
+        return Object.entries(methods).map(([method, _]) => {
+          return `${method.toUpperCase()} ${path}`;
+        });
+      })
+      .flat()
+      .map(formatVariable)
+      .sort();
+
+    // get all paths from the routes config in the format {METHOD} {PATH}
+    const routesConfigPaths = routesConfig
+      .map(({ method, uriPath }) => {
+        return `${method.toUpperCase()} ${uriPath}`;
+      })
+      .sort();
+
+    expect(oaiPaths).toMatchInlineSnapshot(`
+      [
+        "DELETE /tools/:id",
+        "GET /tools",
+        "POST /login",
+        "POST /signup",
+        "POST /tools",
+      ]
+    `);
+    expect(oaiPaths).toEqual(routesConfigPaths);
+  });
+});
